920b2682d7422de00d1bc3f64e02bca6a2770126
ernestotamay <32209953+ernestotamay@users.noreply.github.com>
2021-04-16 15:50:11 -0500

    tabla de multiplicar
    tabla de multiplicar de un numero entero cualquiera; imprime 10 resultados

---
+++ ejemplo-multiplicacion-ciclo-for.js
@@ -0,0 +1,11 @@
+var numero;
+var resultado;
+
+numero = prompt("Escribe un numero: ");
+//convertir texto a numero
+numero = Number(numero);
+
+for (let i=1; i <= 10; i++){
+    resultado = numero * i;
+    console.log(numero + " x " + i + " = " + resultado);
+}
