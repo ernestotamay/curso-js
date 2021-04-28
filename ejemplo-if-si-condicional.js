67d29dc44cc3afe1f79ce793dc69a7df2916e741
ernestotamay <32209953+ernestotamay@users.noreply.github.com>
2021-04-28 14:59:42 -0500

    ejemplo if en js
    uso de sentencia if (si condicional) en javascript

---
+++ ejemplo-if-si-condicional.js
@@ -0,0 +1,14 @@
+//estructura de control if
+//ejemplo para simular acceso con cubrebocas
+
+let cubrebocas = true;
+
+if (cubrebocas == true){
+//opcion1, respuesta VERDADERA si se cumple condicion
+console.log("acceso permitido");
+
+}else{
+//opcion2, respuesta FALSA sino se cumple condicion
+console.log("acceso denegado");
+
+}
