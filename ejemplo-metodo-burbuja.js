20e0d6f1a8bf522dcd801283b1627c9b92986530
ernestotamay <32209953+ernestotamay@users.noreply.github.com>
2021-05-24 17:02:32 -0500

    cambio de variables
    se cambio el identificador de variables por unas mas simples

--- ejemplo-metodo-burbuja.js
+++ ejemplo-metodo-burbuja.js
@@ -1,17 +1,15 @@
-var respaldo
-var lista = [21,64,44,11,67,99,-3];
-var final = lista.length;
-
+var lista = [21,11,-3,5,-100,8,12,-43];
+var tamanioArray = lista.length;
     console.log(`Lista INICIAL: ${lista}`);
-    for (let posicionMenor=0; posicionMenor < final; posicionMenor++){
-        for (let posicionMayor=posicionMenor+1; posicionMayor < final; posicionMayor++){
-            if (lista[posicionMenor]>lista[posicionMayor]){            
-                copiaSeguridad = lista[posicionMayor];
-                lista[posicionMayor] = lista[posicionMenor];
-                lista[posicionMenor] = copiaSeguridad;
+    for (let i=0; i < tamanioArray; i++){
+        for (let j=i+1; j < tamanioArray; j++){   
+            if (lista[i]>lista[j]){            
+                let aux = lista[j];
+                lista[j] = lista[i];
+                lista[i] = aux;
+                //console.log(`intercambios i:lista[${i}]=${lista[i]} - j:lista[${j}]=${lista[j]}`);
             }
             console.log(`intercambios de LISTA: ${lista}`);
         }
     }
-
     console.log(`Lista ORDENADA: ${lista}`);
